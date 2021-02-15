71ad04217675538f11a48d132fbde0ba69867585
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-02-15 15:34:47 +0300

    2-Type Specifier
    prints
    Characters: a A
    Decimal Numbers: 1977
    Space Before:       1977
    Zero Before: 0000001977
    Float Numbers: 3.14
    One string

---
+++ 2-type specifier/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Characters: %c %c \n", 'a', 65);
+    printf("Decimal Numbers: %d \n", 1977);
+    printf("Space Before: %10d \n", 1977);
+    printf("Zero Before: %010d \n", 1977);
+    printf("Float Numbers: %.2f \n", 3.1416);
+    printf("%s \n", "One string");
+
+
+    return 0;
+}
